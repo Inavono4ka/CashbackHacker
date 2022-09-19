cff1164ab11a88d00c5ef411acff716cab2f26e5
Mariia <mariya.cheaptrip@gmail.com>
2022-09-19 22:23:34 +0300

    testng

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,12 @@
+package ru.netology.service;
+
+import org.testng.annotations.Test;
+
+public class BonusServiceTest {
+
+    @Test
+    void calculateBonus() {
+        BonusService service = new BonusService();
+
+    }
+}
