a2e2e4229cc356ccc39e43a4bac0c9e91d27278a
Mariia <mariya.cheaptrip@gmail.com>
2022-09-19 22:41:30 +0300

    correct commit

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,12 +1,26 @@
 package ru.netology.service;
 
-import org.testng.annotations.Test;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
-public class BonusServiceTest {
+public class CashbackHackServiceTest {
 
     @Test
-    void calculateBonus() {
-        BonusService service = new BonusService();
+    public void remain() {
+        CashbackHackService service = new CashbackHackService();
+        int expected = 100;
+        int amount = 900;
+        int actual = service.remain(amount);
+        assertEquals(actual, expected);
 
+    }
+
+    @Test
+    public void remain2() {
+        CashbackHackService service = new CashbackHackService();
+        int expected = 0;
+        int amount = 1000;
+        int actual = service.remain(amount);
+        assertEquals(actual, expected);
     }
 }
